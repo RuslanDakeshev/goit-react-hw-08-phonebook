a7c8a0028ab0e6f211b77017cb77350053336468
RuslanDakeshev <bil08@outlook.com>
2023-01-22 23:12:52 +0100

    summary1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,17 @@ import { Form } from '../components/Form/Form';
 import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Container, Title, Subtitle } from './App.styled';
+import { RegisterForm } from './RegisterForm/RegisterForm';
+import { LoginForm } from './LoginForm/LoginForm';
+import { UserAuthMenu } from './UserAuthMenu/UserAuthMenu';
 
 
 export const App = () => {
   return (
     <Container>
+      <RegisterForm />
+      <LoginForm />
+      <UserAuthMenu/>
       <Title>Phonebook</Title>
       <Form />
       <Subtitle>Contacts</Subtitle>
---
+++ src/redux/auth/auth-selectors.jsx
@@ -0,0 +1,5 @@
+export const selectToken = (state) => state.auth.token;
+
+export const selectName = (state) => state.auth.user.name;
+
+export const selectIsFetchingCurrentUser = (state) => state.auth.isFetchingCurrentUser;
